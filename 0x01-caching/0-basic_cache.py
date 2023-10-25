c4f2aed1051f96034e32988b27dd4975b16bf579
“sitoaustin” <onyedibesixtusna@gmail.com>
2023-10-25 12:34:35 +0100

    Fixed the whitespace pycodestyle error

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -11,7 +11,6 @@ class BasicCache(BaseCaching):
       - Gets the value of the key passed
     """
 
-
     def __init__(self):
         """Initialize and ingerits
         """
@@ -26,7 +25,7 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """Gets the value of the key
-        """ 
+        """
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
