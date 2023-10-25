41e7edb57f54f0385840034cd42916b6aab532c6
“sitoaustin” <onyedibesixtusna@gmail.com>
2023-10-25 11:53:41 +0100

    Basic cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -2,25 +2,31 @@
 """ Basic Class that in herits from
     BaseCaching module
 """
-class BasicCache(BaseCaching):
+from base_caching import BaseCaching
 
+
+class BasicCache(BaseCaching):
     """ BasicCache defines:
       - Assigns keys and values to BaseCashing dictionary
       - Gets the value of the key passed
     """
+
+
     def __init__(self, cache_data):
         """Initialize and ingerits
         """
         super().__init__(cache_data)
+        
     def put(self, key, item):
         """Sets the key-value pair
         """
         if key is None or item is None:
             return
         self.cache_data[key] = item
+
     def get(self, key):
         """Gets the value of the key
-        """
+        """ 
         if not self.cache_data:
             return None
         return self.cache_data[key]
