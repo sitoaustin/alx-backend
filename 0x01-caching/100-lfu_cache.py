90e262e5005d719888cde5ba4dc80b7f5a695c5a
“sitoaustin” <onyedibesixtusna@gmail.com>
2023-10-25 15:21:49 +0100

    testing

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -29,6 +29,7 @@ class LFUCache(BaseCaching):
                     self.cache_data.pop(d_key)
                     self.frequency.pop(d_key)
                     self.cache_data[key] = item
+                    print("DISCARD: {}".format(key))
                     print("DISCARD: {}".format(d_key))
                     return
         # if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
