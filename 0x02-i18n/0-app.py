4a018cc096faadbe1a9cecc326ba4165f98506de
“sitoaustin” <onyedibesixtusna@gmail.com>
2023-10-31 14:12:25 +0100

    Simple flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,7 +1,18 @@
+#!/usr/bin/env python3
+"""
+   Simple flask app
+"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def home_page():
+def home_page() -> str:
+    """
+    routes to the home page
+    """
     return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
