2037b623a8ac15285f95ee33ac9810b466613b5f
“sitoaustin” <onyedibesixtusna@gmail.com>
2023-10-31 16:14:04 +0100

    getting the best locale time

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+   Simple flask app
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, localeselector
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """
+    configuring babel
+    """
+    LANGUAGES = ["en", "fr"]
+
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+@babel.localeselector
+def get_locale():
+    """Get the best locale match for the current request."""
+
+    # Get the user's preferred languages from the Accept-Language header.
+    languages = request.accept_languages
+
+    # Return the best match with our supported languages.
+    return languages.best_match(app.config["LANGUAGES"])
+
+@app.route("/")
+def home_page() -> str:
+    """
+    routes to the home page
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
